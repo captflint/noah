259dd2f74cf1428170256bfd8b1ad308fac9e3a0
James Stephenson <c4p7.fl1n7@gmail.com>
2014-06-15 11:31:02 -0400

    started experimenting with curses

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
-import lzma
-with lzma.open('webster.txt.xz', 'rt') as w:
-    webster = w.read()
-query = input('Enter a word to look up: ')
-query = '\n' + query.upper() + '\n'
-start = webster.find(query)
-if start == -1:
-    print('Word not found')
-else:
-    while True:
-        line = ''
-        while webster[start] != '\n':
-            line += webster[start]
-            start += 1
-        input(line)
-        line = ''
-        start += 1
+import curses
+
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+
+screen.getch()
+curses.flash()
+screen.getch()
+
+curses.nocbreak()
+screen.keypad(False)
+curses.echo()
+curses.endwin()
