122728497bd799306164f4c85b2cee3aeedbca69
James Stephenson <c4p7.fl1n7@gmail.com>
2014-06-15 11:51:54 -0400

    more curses learning

--- test.py
+++ test.py
@@ -6,7 +6,38 @@ curses.cbreak()
 screen.keypad(True)
 
 screen.getch()
-curses.flash()
+screen.scrollok(True)
+screen.addstr("""one
+two
+three
+four
+five
+six
+seven
+eight
+nine
+ten
+eleven
+twelve
+thirteen
+fourteen
+fifteen
+sixteen
+seventeen
+eighteen
+nineteen
+twenty
+twenty one
+twenty two
+twenty three
+twenty four
+twenty five
+twenty six
+twenty seven
+twenty eight
+twenty nine
+thirty
+""")
 screen.getch()
 
 curses.nocbreak()
