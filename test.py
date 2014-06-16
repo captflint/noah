078235be4500acf4995aef5507dff80ff2cb61d5
James Stephenson <c4p7.fl1n7@gmail.com>
2014-06-16 09:37:16 -0400

    successfully implemented scrolling function

--- test.py
+++ test.py
@@ -10,10 +10,24 @@ def scrollythingy(shitTonOfText):
     screen.clear()
     height = screen.getmaxyx()[0]
     command = 0
+    offset = 0
+    screen.scrollok(True)
     while chr(command) not in 'Qq':
-        screen.addstr(str(command))
-        command = screen.getch()
         screen.clear()
+        dispStr = ""
+        lineCounter = 0
+        for char in shitTonOfText:
+            if char == "\n":
+                lineCounter += 1
+            if lineCounter >= offset and lineCounter < offset + height:
+                dispStr += char
+        screen.addstr(dispStr)
+        command = screen.getch()
+        if command == 259: #Up arrow key
+            if offset > 0:
+                offset -= 1
+        elif command == 258: #Down arrow key
+            offset += 1
 
 longstring = """one
 two
@@ -46,7 +60,9 @@ twenty eight
 twenty nine
 thirty
 """
-scrollythingy('blahblahblah')
+scrollythingy(longstring)
+scrollythingy('blah')
+scrollythingy('blah\nblah\nblah')
 
 curses.nocbreak()
 screen.keypad(False)
